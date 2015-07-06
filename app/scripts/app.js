3ecd2c247eaddcb8ac5173e1bdaa4a4da3a04f97
Nader Toukabri <nader.toukabri@gmail.com>
2015-07-06 05:47:00 +0100

    wip

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,7 +7,7 @@ Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
 */
 
-(function(document) {
+(function (document) {
   'use strict';
 
   // Grab a reference to our auto-binding template
@@ -15,27 +15,30 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
 
-  app.displayInstalledToast = function() {
-    document.querySelector('#caching-complete').show();
-  };
+  // app.displayInstalledToast = function () {
+  //   document.querySelector('#caching-complete').show();
+  // };
 
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
-  app.addEventListener('dom-change', function() {
+  app.addEventListener('dom-change', function () {
     console.log('Our app is ready to rock!');
   });
 
-  // See https://github.com/Polymer/polymer/issues/1381
-  window.addEventListener('WebComponentsReady', function() {
-    // imports are loaded and elements have been registered
-  });
-
   // Close drawer after menu item is selected if drawerPanel is narrow
-  app.onMenuSelect = function() {
+  app.onMenuSelect = function () {
     var drawerPanel = document.querySelector('#paperDrawerPanel');
-    if (drawerPanel.narrow) {
+    if (drawerPanel && drawerPanel.narrow) {
       drawerPanel.closeDrawer();
     }
   };
 
 })(document);
+
+window.addEventListener('DOMContentLoaded', function () {
+  setTimeout(function () {
+    document.dispatchEvent(new CustomEvent('WebComponentsReady', {
+      bubbles: true
+    }));
+  });
+});
